01cdedc72a7e5595dbcf6b44ce75d5107dd341d7
Elza Oliveira <elza.souza93@gmail.com>
2024-03-08 13:22:17 +0000

    first commit

---
+++ src/main/java/com/logica/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.logica;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World Elza!");
+		System.out.print("Elza");
+		System.out.println("Souza Oliveira");
+		
+		System.out.println("");
+		System.out.println("Lógica de programação");
+		
+		Variaveis.imprime("vai imprimir aqui");
+
+	}
+
+}
---
+++ src/main/java/com/logica/Variaveis.java
@@ -0,0 +1,25 @@
+package com.logica;
+
+public class Variaveis {
+
+	//função ou método
+	public static void imprime(String valor) {
+			System.out.println(valor);
+	}
+	
+	public static void main(String[] args) {
+		
+		//Criação de uma variável do tipo string
+		String curso1 = "Lógica de programação";
+		String curso2 = "Automação de testes com WebDriver";
+		
+		imprime(curso1 +"-"+curso2);
+		imprime("06/02/2024");
+		imprime("Elza");
+		imprime("4505");
+		
+	
+	}
+
+}
+	
